bfe9ac0c3490cde43368fa39e3fc68f4c4a65b95
mmiicchheellllee <47775119+mmiicchheellllee@users.noreply.github.com>
2019-02-26 17:03:47 +0800

    Create q2_calc_cylinder_volume.py

---
+++ p01/q2_calc_cylinder_volume.py
@@ -0,0 +1,17 @@
+'''
+2 (Computing the volume of a cylinder)
+C reads in the radius and length of a cylinder and computes its volume using the following formulae:
+area = radius * radius * pi
+volume = area * length '''
+
+from math import pi
+# Get input
+radius = float(input("Enter radius: "))
+length = float(input("Enter length: "))
+
+# Compute volume
+area = radius * radius * pi
+volume = area * length
+
+# Output result
+print("Volume of cylinder: ""{0:.1f}".format(volume))
