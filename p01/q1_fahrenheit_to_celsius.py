5827f9d03a04abc76a9e98dc3dbc0adee4be11ca
mmiicchheellllee <47775119+mmiicchheellllee@users.noreply.github.com>
2019-02-26 17:02:49 +0800

    Update q1_fahrenheit_to_celsius.py

--- p01/q1_fahrenheit_to_celsius.py
+++ p01/q1_fahrenheit_to_celsius.py
@@ -1,14 +1,12 @@
 '''
 1. (Converting Fahrenheit to Celsius)
-Write a program q1_fahrenheit_to_celsius.py that reads a Fahrenheit degree in double (floating point / decimal) from standard input, 
-then converts it to Celsius and displays the result in standard output. The formula for the conversion is as 
-follows: celsius = (5/9) * (fahrenheit - 32)'''
+Write a program q1_fahrenheit_to_celsius.py that reads a Fahrenheit degree in double (floating point / decimal) from standard input, then converts it to Celsius and displays the result in standard output. The formula for the conversion is as follows: celsius = (5/9) * (fahrenheit - 32)'''
 
 # Get input
-fahrenheit = int(input("Enter temperature in Fahrenheit"))
+fahrenheit = int(input("Enter temperature in Fahrenheit: "))
 
 # Compute celsius temperature
 celsius = (5/9) * (fahrenheit - 32)
 
 # Output result
-print("{0:.1f}".format(celsius))
+print("Temperature in Celsius: ""{0:.1f}".format(celsius))
